889ce8377d633b3ecba348e9f81617ccb92e0a1e
Dipankar <drupd17@gmail.com>
2025-03-18 18:16:49 +0530

    remove unwanted code

--- lib/app/presentation/views/registration/show_map_page.dart
+++ lib/app/presentation/views/registration/show_map_page.dart
@@ -1,159 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
-// import 'package:geolocator/geolocator.dart';
-
-// class RegistrationMapPage extends StatefulWidget {
-//   const RegistrationMapPage({super.key});
-
-//   @override
-//   State<RegistrationMapPage> createState() => _RegistrationMapPageState();
-// }
-
-// class _RegistrationMapPageState extends State<RegistrationMapPage> {
-//   static const LatLng _initialPosition = LatLng(12.9716, 77.5946);
-
-//   late GoogleMapController _mapController;
-//   Marker? _marker;
-//   Position? _currentLocation;
-
-//   Future<Position> _getCurrentPosition() async {
-//     bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-//     if (!serviceEnabled) {
-//       return Future.error('Location services are disabled.');
-//     }
-
-//     LocationPermission permission = await Geolocator.checkPermission();
-//     if (permission == LocationPermission.denied) {
-//       permission = await Geolocator.requestPermission();
-//       if (permission == LocationPermission.denied) {
-//         return Future.error('Location permissions are denied');
-//       }
-//     }
-
-//     if (permission == LocationPermission.deniedForever) {
-//       return Future.error('Location permissions are permanently denied.');
-//     }
-
-//     return await Geolocator.getCurrentPosition();
-//   }
-
-//   void _onMapCreated(GoogleMapController controller) {
-//     _mapController = controller;
-//   }
-
-//   void _updateMarker(LatLng pos) {
-//     setState(() {
-//       _marker = Marker(
-//         markerId: const MarkerId('selected_location'),
-//         position: pos,
-//         draggable: true,
-//         icon: BitmapDescriptor.defaultMarker,
-//         infoWindow: const InfoWindow(
-//           title: 'Selected Location',
-//         ),
-//       );
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // fetch the username arg from the prev screen:
-//     final arg = ModalRoute.of(context)?.settings.arguments as Map?;
-//     // final username = arg?['username'] ?? 'User';
-
-//     return Scaffold(
-//       appBar: AppBar(
-//         leading: IconButton(
-//           onPressed: () {
-//             Navigator.pop(context);
-//           },
-//           icon: const Icon(Icons.arrow_back),
-//         ),
-//         title: Text(
-//           'Enter Location',
-//         ),
-//       ),
-//       body: Padding(
-//         padding: EdgeInsets.all(10),
-//         child: Column(
-//           children: [
-//             Text(
-//               'Please select preferred location for classes',
-//             ),
-//             SizedBox(
-//               height: 5,
-//             ),
-//             Expanded(
-//               child: GoogleMap(
-//                 onMapCreated: _onMapCreated,
-//                 initialCameraPosition: const CameraPosition(
-//                   target: _initialPosition,
-//                   zoom: 13,
-//                 ),
-//                 markers: _marker != null ? {_marker!} : {},
-//                 onTap: (LatLng pos) {
-//                   _updateMarker(pos);
-//                 },
-//               ),
-//             ),
-//             SizedBox(height: 20),
-//             ElevatedButton(
-//               onPressed: () {
-//                 // Handle location selection
-//                 if (_marker != null) {
-//                   Navigator.pop(context, {
-//                     'location': {
-//                       'lat': _marker!.position.latitude,
-//                       'lng': _marker!.position.longitude
-//                     }
-//                   });
-//                 } else {
-//                   ScaffoldMessenger.of(context).showSnackBar(
-//                     SnackBar(content: Text('Please select a location')),
-//                   );
-//                 }
-//               },
-//               child: Text('Confirm Location'),
-//             ),
-//             SizedBox(height: 10),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () async {
-//           try {
-//             _currentLocation = await _getCurrentPosition();
-
-//             if (_currentLocation != null) {
-//               _updateMarker(
-//                 LatLng(
-//                   _currentLocation!.latitude,
-//                   _currentLocation!.longitude,
-//                 ),
-//               );
-
-//               _mapController.animateCamera(
-//                 CameraUpdate.newLatLngZoom(
-//                   LatLng(
-//                     _currentLocation!.latitude,
-//                     _currentLocation!.longitude,
-//                   ),
-//                   15,
-//                 ),
-//               );
-//             }
-//           } catch (e) {
-//             ScaffoldMessenger.of(context).showSnackBar(
-//               SnackBar(content: Text(e.toString())),
-//             );
-//           }
-//         },
-//         child: const Icon(Icons.my_location),
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -304,6 +148,13 @@ class _RegistrationMapPageState extends State<RegistrationMapPage> {
             children: [
               const Text(
                 'Please select preferred location for classes',
+              ),
+              const Text(
+                'Note: Selecting your preferred location is one time process. You cannot change it later.',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               const SizedBox(
                 height: 5,
